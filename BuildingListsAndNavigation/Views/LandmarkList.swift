d245fba3c88adeab090f4f743876201193d6fb14
Rodrigo Brandão <k-noon@hotmail.com>
2021-04-13 01:38:07 -0300

    Section 5 - Tornando a lista dinamica

---
+++ BuildingListsAndNavigation/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  BuildingListsAndNavigation
+//
+//  Created by Rodrigo Brandão on 13/04/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
