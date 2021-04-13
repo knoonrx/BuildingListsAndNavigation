e11d764e00e5e333b1b3d4da0b74abcd29429f69
Rodrigo Brandão <k-noon@hotmail.com>
2021-04-13 02:18:49 -0300

    Seção 7 - Enviar dados para a view

--- BuildingListsAndNavigation/Views/MapView.swift
+++ BuildingListsAndNavigation/Views/MapView.swift
@@ -8,6 +8,8 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: -22.620_833, longitude: -52.171667),
         span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
@@ -16,10 +18,22 @@ struct MapView: View {
     
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                _setRegion(coordinate)
+            }
     }
+    
+    private func _setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+    
 }
+
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
     }
 }
