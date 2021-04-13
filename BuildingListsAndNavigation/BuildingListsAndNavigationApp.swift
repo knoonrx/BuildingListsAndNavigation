8f18259df88dcbd71585d1558d77b9271488e53f
Rodrigo Brandão <k-noon@hotmail.com>
2021-04-12 23:28:27 -0300

    Initial Commit

---
+++ BuildingListsAndNavigation/BuildingListsAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListsAndNavigationApp.swift
+//  BuildingListsAndNavigation
+//
+//  Created by Rodrigo Brandão on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct BuildingListsAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
